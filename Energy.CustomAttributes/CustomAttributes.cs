f4d26fe1e9e7c96b1761cc869d995ef3b84d6183
Артём Нестерков <nesterkov02@gmail.com>
2020-08-13 23:55:14 +0300

    Добавьте файлы проекта.

---
+++ Energy.CustomAttributes/CustomAttributes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Energy.Plugin
+{
+    public class CustomAttributes
+    {
+        [AttributeUsage(AttributeTargets.Class)]
+        public class Info : Attribute
+        {
+            public string Author { get; }
+            public string Title { get; }
+            public string Version { get; }
+            public Info(string Author, string Title,string Version)
+            {
+                this.Author = Author;
+                this.Title = Title;
+                this.Version = Version;
+            }
+        }
+        [AttributeUsage(AttributeTargets.Class)]
+        public class Description : Attribute
+        {
+            public string Desc { get; }
+            public Description(string Desc)
+            {
+                this.Desc = Desc;
+            }
+        }
+    }
+}
---
+++ Energy.CustomAttributes/Plugin.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Reflection;
+
+namespace Energy.Plugin
+{
+   public class Plugin
+    {
+        private BindingFlags Flags = BindingFlags.Instance | BindingFlags.GetProperty | BindingFlags.SetProperty | BindingFlags.GetField | BindingFlags.SetField | BindingFlags.NonPublic | BindingFlags.Public;
+        string name;
+        public string Name
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(this.Name) || this.name == base.GetType().Name) this.name = value;
+            }
+            get
+            {
+                return this.name;
+            }
+        }
+        public string Author;
+        public string Title;
+        public string Desc;
+        public string Version;
+        public Plugin() 
+        {
+            Name = GetType().Name;
+            Author = "Artemka";
+            Title = "default plugin";
+            Desc = "test";
+            Version = "1.0.0";
+        }         
+        public void GetInfoPlugin()
+        {
+            var CustomInfo = GetType().GetCustomAttributes(typeof(CustomAttributes.Info), true);
+            if (CustomInfo.Length != 0)
+            {
+                var Info = CustomInfo[0] as CustomAttributes.Info;
+                Author = Info.Author;
+                Title = Info.Title;
+                Version = Info.Version;
+                var CustomDescription = GetType().GetCustomAttributes(typeof(CustomAttributes.Description), true);
+                if (CustomDescription.Length != 0)
+                {
+                    var Description = CustomDescription[0] as CustomAttributes.Description;
+                    Desc = Description.Desc;
+                }
+            }         
+        }
+        public object CallHook(string hook, params object[] args) =>  GetType().GetMethod(hook, Flags).Invoke(this, args);     
+    }
+}
